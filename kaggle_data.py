3209ae89ea7000270f156d91d2281e62db7834de
StefanJansen <sjansen@post.harvard.edu>
2016-05-09 01:44:24 -0400

    kaggle download update

--- kaggle_data.py
+++ kaggle_data.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 __author__ = 'Stefan Jansen'
 
-from os import environ
+import shutil
+from os import environ, listdir
 from os.path import join
 from urllib.parse import urljoin
 
-import shutil
 import requests
 from bs4 import BeautifulSoup as soup
+from pandas import *
 
 analysis_dir = '/home/kaggle/analysis'
 
-def download(competition='expedia-hotel-recommendations', filetype='.gz'):
+
+def download_data(competition='expedia-hotel-recommendations', filetype='.gz'):
     kaggle_url = 'https://www.kaggle.com'
 
     login_url = '/'.join(s.strip('/') for s in [kaggle_url, 'account', 'login'])
@@ -32,6 +34,14 @@ def download(competition='expedia-hotel-recommendations', filetype='.gz'):
                 shutil.copyfileobj(response.raw, f)  # download uncompressed
 
 
-if __name__ == '__main__':
-    download()
+def data_to_hdf():
+    with HDFStore(join(analysis_dir, 'source.h5')) as store:
+        for f in [l for l in listdir(analysis_dir) if l.endswith('.csv')]:
+            file_name = f.split('.')[0]
+            df = read_csv(join(analysis_dir, f))
+            store.put(file_name, df)
 
+
+if __name__ == '__main__':
+    download_data()
+    data_to_hdf()
